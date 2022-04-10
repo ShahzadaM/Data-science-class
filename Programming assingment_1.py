03ab75dd471e93f3804c589fed55f88ea09fdade
ShahzadaM <102745784+ShahzadaM@users.noreply.github.com>
2022-04-10 22:39:26 +0530

    Add files via upload

---
+++ Programming assingment_1.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+s= "Hello World"
+
+
+# In[2]:
+
+
+s
+
+
+# In[9]:
+
+
+a= int(input("enter your first number "))
+b = int(input("enter your second number "))
+
+
+# In[10]:
+
+
+sum = a+b
+sub = a-b
+product = a*b
+divison = a/b
+
+
+# In[11]:
+
+
+sum
+
+
+# In[12]:
+
+
+sub
+
+
+# In[13]:
+
+
+product
+
+
+# In[14]:
+
+
+divison
+
+
+# In[20]:
+
+
+# area of triangle
+length = int(input("enter your length "))
+breadth = int(input("enter your breadth "))
+Area = length*breadth
+print('area of triangle is', Area)
+
+
+# In[22]:
+
+
+#Program to swap variable
+a= 10
+b= 23
+a,b = b,a
+print('value of a is', a)
+print('value of b is', b)
+
+
+# In[ ]:
+
+
+
+
